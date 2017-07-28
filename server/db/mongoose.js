a93f5b427db40cf725e3c5db1d0c078730a6dba8
Philip Balbas <philip.balbas@gmail.com>
2017-07-28 12:39:06 +0800

    incorporated mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/TodoApp', {
+
+mongoose.connect('mongodb://yves:yves@ds127063.mlab.com:27063/notes-api', {
   useMongoClient: true
 })
+
+// mongoose.connect('mongodb://localhost:27017/TodoApp', {
+//   useMongoClient: true
+// })
 
 module.exports = { mongoose }
